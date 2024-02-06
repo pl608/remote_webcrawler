b2477d01c30425f8510befca740af7c360742fde
pl608 <78400949+pl608@users.noreply.github.com>
2024-02-06 14:16:36 -0800

    Update mqtt.py

--- mqtt.py
+++ mqtt.py
@@ -3,22 +3,23 @@ import random
 from paho.mqtt import client as mqtt_client
 from hashlib import sha512, md5
 from time import time
-broker_ = 'public.mqtthq.com'
-port_ = 1883
+broker_ = '9fcd632e24e3417ba95802e5a6165679.s1.eu.hivemq.cloud'
+port_ = 8883
 def on_message_(client, userdata, msg):
     print(userdata)
     print(msg.payload.decode())
 
 class Network:
-    def __init__(self, broker=broker_, port=port_, topic=sha512(str(int(time()/60/60/24)).encode('utf-8')).hexdigest(), on_message=on_message_) -> None:
+    def __init__(self, broker=broker_, port=port_, topic=sha512(str(int(time()/60/60/24)).encode('utf-8')).hexdigest(), on_message_=on_message_, usr='webcrawler', pwd='Crawler1234') -> None:
         self.device_name = md5(f"{socket.gethostname()}{str(random.randint(1, 100))}".encode('utf-8')).hexdigest()[:4]
         self.broker = broker
         self.port = port
         self.topic = topic
-        self.on_message = lambda cli, usr, msg: on_message(self, cli, usr, msg)
-        self.connect()
+        self.on_message = lambda cli, usr, msg: on_message_(self, cli, usr, msg)
+        self.connect(usr, pwd)
         #self.subscribe()
-    def connect(self):
+    def connect(self, usr, pwd):
+        
         def on_connect(client, userdata, flags, rc):
             if rc == 0:
                 print("Connected to MQTT Broker!")
@@ -27,8 +28,10 @@ class Network:
 
         client = mqtt_client.Client(self.device_name)
         client.on_connect = on_connect
+        client.tls_set(tls_version=mqtt_client.ssl.PROTOCOL_TLS)
+        client.username_pw_set(usr or self.device_name, pwd or None)
         client.connect(self.broker, self.port)
-        client.username_pw_set(self.device_name, None)
+        client.publish('start')
         self.client = client
 
     def subscribe(self, topic=None):
