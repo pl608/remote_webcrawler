ddcf1ab98532101deadfaeab0a86cb9c2ee2ed66
pl608 <78400949+pl608@users.noreply.github.com>
2024-02-06 16:32:59 -0800

    Update main_network.py

--- main_network.py
+++ main_network.py
@@ -38,9 +38,9 @@ class Website:
     def __setattr__(self, __name: str, __value: Any) -> None:
         super().__setattr__(__name, __value)
 class Data():
-    def __init__(self, directory='./urls') -> None:
+    def __init__(self, directory='D:\\urls') -> None:
         self.dir = directory
-        self.unpros = []#['https://www.wikipedia.org/','']]
+        self.unpros = [['https://www.wikipedia.org/','']]
         self.urls = {}
         for root, dirs, files in walk(self.dir):
             for file in files:
@@ -48,20 +48,24 @@ class Data():
                     zip = open(path.join(root, file), 'r')
                     data = loads(zip.read())
                     data['scanned'] = data.get('scanned', False)
+                    data['url'] = data['url'].split('?',1)[0]
                     self.urls.update({data['url']:Website(**data)})
                     if data.get('scanned', False) == False:
                         self.unpros.append([data['url'], data['parent_url']])
     def trigger_save(self,url):
-        path_ = path.join(self.dir, md5(url.encode('utf-8')).hexdigest()[:3],md5(url.encode('utf-8')).hexdigest()[:16])
-        try: mkdir(path.join(self.dir, path.basename(path_)[:3]))
+        path_ = path.join(self.dir, md5(url.encode('utf-8')).hexdigest()[:2],md5(url.encode('utf-8')).hexdigest()[:16])
+        try: mkdir(path.join(self.dir, path.basename(path_)[:2]))
         except: pass
         f = open(path_, 'w')
         f.write(dumps(self.urls[url].__dict__))
         f.close()
     def add(self, cls):
+        cls.url = cls.url.split('?',1)[0]
         if cls.url in self.urls:
             self.urls[cls.url].alt_parents += [cls.parent_url]
+            self.urls[cls.url].alt_parents = list(set(self.urls[cls.url].alt_parents))
             self.urls[cls.url].alt_runner += [cls.runner]
+            self.urls[cls.url].alt_parents = list(set(self.urls[cls.url].alt_runner))
             self.trigger_save(cls.url)
         else:
             self.unpros.append([cls.url, cls.parent_url])
@@ -94,7 +98,7 @@ def on_message(net, cli, usr_data, msg):
                     d.trigger_save(d_[0])
                 except: pass
                 
-                net.publish(sender+b64encode(dumps([d_[0], d_[1]]).encode('utf-8')).decode('utf-8'))
+                net.publish(sender+b64encode(dumps([d_[0].split('?',1)[0], d_[1].split('?',1)[0]]).encode('utf-8')).decode('utf-8'))
             except Exception as e:
                 _ = e
             print('[REQUEST] ',sender)
